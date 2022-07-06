c3a4a8002f04e274dee0bdef28290e438d39debe
Thomas Konietzny <thomas.konietzny@fh-erfurt.de>
2022-07-06 13:48:03 +0200

    cleared out unneeded code + formatting

--- db.js
+++ db.js
@@ -1,17 +1,25 @@
 const { MongoClient } = require("mongodb");
-// Replace the uri string with your connection string.
+
 const uri =
-"mongodb+srv://admin:admin@redspikyfarm.xvdee.mongodb.net/?retryWrites=true&w=majority"
+"mongodb+srv://admin:admin@redSpikyFarm.xvdee.mongodb.net/redSpikyFarm?retryWrites=true&w=majority"
 const client = new MongoClient(uri);
 async function run() {
   try {
-    const database = client.db('redspikyfarm');
-    const tools = database.collection('Tools');
-    // Query for a tool that has the name "Hoe"
-    const query = { name: "Hoe" };
-    const tool = await tools.findOne(query);
+    const database = client.db('redSpikyFarm');
     
-    console.log(tool);
+    
+    // // Query for a tool that has the name "Hoe"
+    //const tools = database.collection('Tools');
+    // const query = { name: "Hoe" };
+    // const tool = await tools.findOne(query);
+    // console.log(tool);
+
+    // //Get all Collectioncursors
+    //const collections = await database.collections();
+    //console.log(collections);
+    
+    
+    
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
